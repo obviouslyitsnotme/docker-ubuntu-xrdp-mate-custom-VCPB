abaa3bb492928fa7a955c9ccefb97d6a33d28082
obviouslyitsnotme <77265247+obviouslyitsnotme@users.noreply.github.com>
2021-01-20 18:28:17 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ $UID -eq 0 ]; then
-user=$1vcpb
+user=$1vcpb 
 exec su "$user" "$0" -- "$@"
 fi
 echo "This will be run from user $UID"
-mv /home/Telegram/Telegram /home/vcpb/Desktop/Telegram #Move Telegram to desktop
+mv /home/Telegram/Telegram /home/vcpb/Desktop/Telegram #Move Telegram to vcpb/Desktop
 pactl load-module module-null-sink sink_name=MySink #loading MySink
 pactl set-default-sink MySink #setting Mysink as default Mic
 cd /home/vcbot 
